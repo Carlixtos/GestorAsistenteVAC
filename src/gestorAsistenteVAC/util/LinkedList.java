ed14e35d6bc407dd8f56aea118c3d2c5896b3f7a
Carlos Rios <carlosandresrio33@hotmail.com>
2021-04-17 19:04:24 -0500

    Se mejoraron los comentarios de las clases Node y LinkedList. Los metodos de LinkedList ya no toman información como parametros sino nodos

--- src/gestorAsistenteVAC/util/LinkedList.java
+++ src/gestorAsistenteVAC/util/LinkedList.java
@@ -1,26 +1,56 @@
 package gestorAsistenteVAC.util;
 
+/**
+ * Clase generica lista enlazada, una implementación de una lista doblemente enlazada. 
+ * @author Dafty-Punky-Boy
+ * @version 1.0, 17/04/2021
+ * 
+ * @param tail Referencia al primer nodo de la lista.
+ * @param head Referencia al ultimo nodo de la lista. 
+ */
 public class LinkedList<T> {
 	Node<T> head;
 	Node<T> tail;
 	
+	/**
+	 * Crea una lista vacía.
+	 */
 	LinkedList(){
 		head = null;
 		tail = null;
 	}
 	
-	public void addFront(T data) {
-		Node<T> nodeTemp = new Node<T>(data);
-		
+	/**
+	 * Agrega un nodo almacenando un dato de tipo T al frente de la lista.
+	 * @param node El nodo de tipo T que se desea agregar.  
+	 */
+	public void addFront(Node<T> node) {
 		if(tail == null) {
-			head = nodeTemp;
+			head = node;
 			tail = head;
 		}
 		
 		else {
-			nodeTemp.next = head;
-			head.previous = nodeTemp;
-			head = nodeTemp;
+			node.next = head;
+			head.previous = node;
+			head = node;
 		}
-	} 
+	}
+	
+	/**
+	 * Agrega un nodo almacenando un dato de tipo T al final de la lista.
+	 * @param node El nodo de tipo T que se desea agregar.
+	 */
+	public void addBack(Node<T> node) {
+		if(tail == null) {
+			head = node;
+			tail = head;
+		}
+		
+		else {
+			node.previous = tail;
+			tail.next = node;
+			tail = node;
+		}
+	}
 }
--- src/gestorAsistenteVAC/util/Node.java
+++ src/gestorAsistenteVAC/util/Node.java
@@ -4,23 +4,23 @@ package gestorAsistenteVAC.util;
  * Clase generica nodo, pensada para ser utilizada en la implementación de listas. 
  * @author Dafty-Punky-Boy
  * @version 1.0, 17/04/2021
+ * 
+ * @param next Referencia al siguiente nodo de la lista.
+ * @param previous Referencia al nodo previo de la lista.
+ * @param data El dato de tipo T que almacena el nodo.
  */
-
 public class Node<T> {
 	T data;
 	Node<T> next;
 	Node<T> previous;
 	
+	/**
+	 * Crea un nodo que almacena un dato de tipo T.
+	 * @param data El dato de tipo T.
+	 */
 	Node(T data) {
 		this.data = data;
 		next = null;
 		previous = null;
-	}
-	
-	/**
-	 * Crea un nodo que almacena un dato de tipo T.
-	 * @param next Referencia al siguiente nodo de la lista.
-	 * @param previous Referencia al nodo previo de la lista.
-	 * @param data El dato de tipo T que almacena el nodo.
-	 */
+	}	
 }
